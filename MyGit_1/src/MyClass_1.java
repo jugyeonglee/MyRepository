fda07d40513f32f9d88689c06c6ca807eff32e41
jugyoenglee <juju967400@naver.com>
2021-06-21 06:24:13 +0900

    test2

--- MyGit_1/src/MyClass_1.java
+++ MyGit_1/src/MyClass_1.java
@@ -1,4 +1,7 @@
 
 public class MyClass_1 {
-public static void main(String args[]) { System.out.println("Hello Git!");
-} }
+public static void main(String args[]) { 
+	System.out.println("Hello Git!");
+	System.out.println("This is Git test");
+   }
+}
