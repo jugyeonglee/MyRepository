85b22882990ab072143f0f4a43789bdb3fbe9688
jugyoenglee <juju967400@naver.com>
2021-06-21 06:40:33 +0900

    test3

--- MyGit_1/src/MyClass_1.java
+++ MyGit_1/src/MyClass_1.java
@@ -3,5 +3,7 @@ public class MyClass_1 {
 public static void main(String args[]) { 
 	System.out.println("Hello Git!");
 	System.out.println("This is Git test");
+	System.out.println("MyHello modified");
+	
    }
 }
--- MyGit_1/src/MyHello.java
+++ MyGit_1/src/MyHello.java
@@ -1,6 +1,8 @@
 public class MyHello {
 public static void main(String args[]) { 
-	System.out.println("Hello Git!");
-	System.out.println("This is Git test2");
+	System.out.println("Hello Git!1");
+	System.out.println("Hello Git2!");
+	System.out.println("Hello Git3!");
+	
    }
 }
