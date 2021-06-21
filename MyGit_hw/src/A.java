f7425f9fc188ffc1e62cb685e9125e1bec6155ec
jugyoenglee <juju967400@naver.com>
2021-06-21 19:59:46 +0900

    modified for A.java master branch

--- MyGit_hw/src/A.java
+++ MyGit_hw/src/A.java
@@ -3,7 +3,7 @@ public class A {
 public static void main(String args[]) { 
 	System.out.println("Hello Git!");
 	System.out.println("This is Git test for A");
-	System.out.println("MyHello modified");
+	System.out.println("MyHello modified for master");
 	
    }
 }
